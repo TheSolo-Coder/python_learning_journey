45760d5d8e8cb78a654b0448c7d36ec8d959d0b9
Muhammad Ali <zyfora.dev@gmail.com>
2026-03-13 09:51:46 +0500

    Add practice.py for Day 02 - Variables practice
    Practiced Python variables and print output for Day 02 of my learning journey.

---
+++ day_02_variables/practice.py
@@ -0,0 +1,10 @@
+# Day 02 - practicing variables 
+python_greetings = "Welcome To Python"
+name = "Ali"
+user_response = "Thanks Python"
+
+# Print the output 
+print(python_greetings,name)
+print(user_response)
+
+
