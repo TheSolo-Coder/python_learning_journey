f58c5f5dfe52a52d1aa6777c1d1945b67cc5c6a2
Muhammad Ali <zyfora.dev@gmail.com>
2026-03-14 08:24:48 +0500

    Update practice.py for Day 03 - Data Types
    Updated Day 03 - practice.py with corrected examples and minor fixes

--- day_03_data_types/practice.py
+++ day_03_data_types/practice.py
@@ -1,12 +1,11 @@
 # Day 3 - Data Types Practice
-
 name = "Ali"
 age = 21
 height = 5.8
 is_student = True
 
 # Print output with explanation
-print(type(name), "Ali")
-print(type(age), "is 21 years old")
+print(type(name), name)  # <class 'str'> Ali
+print(type(age), age, "years old")  # <class 'int'> 21 years old
 print(type(height), "height is", height, "inch")
-print(type(is_student), "True")
+print(type(is_student), "Student?", is_student)
